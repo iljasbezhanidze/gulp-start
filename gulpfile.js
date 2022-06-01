8d77b6eb0cb89a4825e9398e2cb080a36405fb19
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2022-06-01 08:36:36 +0300

    start build tempaltes

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,99 @@
+const importLibs = [
+    'node_modules/@fancyapps/ui/dist/fancybox.css',
+    'node_modules/swiper/swiper-bundle.css'
+]
+
 const { src, dest, watch, series, parallel } = require('gulp'),
     browserSync = require('browser-sync').create(),
-
-//плагіны
-    fileInclude  = require('gulp-file-include'),
-    sass         = require('gulp-dart-sass'),
-    plumber      = require('gulp-plumber'),
-    notify       = require('gulp-notify'),
-    concat       = require('gulp-concat'),
-    mediaQueries = require('gulp-group-css-media-queries'),
+    fileInclude = require('gulp-file-include'),
+    sass = require('gulp-dart-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    ttf2woff2    = require('gulp-ttftowoff2'),
-    cleanCSS     = require('gulp-clean-css'),
-    shorthand    = require('gulp-shorthand'),
-    // shorthand    = require('concat-css-properties'),
-    del          = require('del'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    // purgecss = require('gulp-purgecss'),
+    mediaQueries = require('gulp-group-css-media-queries'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    ttf2woff2 = require('gulp-ttftowoff2'),
+    del = require('del')
 
-//обробка html
-    html = () => {
-        return src('./src/*.html')
-            .pipe(plumber({
-                errorHandler: notify.onError()
-            }))
-            .pipe(fileInclude())
-            .pipe(dest('./dist'))
-            .pipe(browserSync.stream());
-    },
+    //DEV TASKS------------------------------
+const html = () => {
+    return src('./src/*.html')
+        .pipe(plumber({
+            errorHandler: notify.onError()
+        }))
+        .pipe(fileInclude())
+        .pipe(dest('./dist'))
+        .pipe(browserSync.stream());
+    }
 
-//обробка стилів
-    styles = () => {
-        return src([
+const styles = () => {
+    return src([
             './src/scss/base/main.{scss, saas}',
             './src/scss/**.{scss, sass, css}',
-            './src/scss/blocks/**.{scss, sass, css}'
-            ])
-            .pipe(sourcemaps.init())
-            .pipe(sass().on('error', notify.onError()))
-            .pipe(concat('main.min.css'))
-            .pipe(autoprefixer({
-                overrideBrowserslist: ['last 5 versions']
-            }))
-            .pipe(mediaQueries())
-            // .pipe(shorthand())
-            .pipe(cleanCSS({level: 2}))
-            .pipe(sourcemaps.write('.'))
-            .pipe(dest('./dist/css'))
-            .pipe(browserSync.stream());
-    },
+            './src/scss/blocks/**.{scss, sass, css}',
+            ...importLibs
+        ])
+
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', notify.onError()))
+        .pipe(concat('main.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 5 versions']
+        }))
+        .pipe(mediaQueries())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./dist/css'))
+        .pipe(browserSync.stream());
+}
+
+const scripts = () => {
+    return src('./src/js/main.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./dist/js'))
+        .pipe(browserSync.stream());
+}
+
+const images = () => {
+    return src('./src/img/**.*')
+        .pipe(dest('./dist/img'))
+}
+
+const fonts = () => {
+    return src('./src/fonts/**.ttf')
+        .pipe(ttf2woff2())
+        .pipe(dest('./dist/fonts/'))
+}
+
+const clear = () => del('./dist')
 
 
-//скріпты
-    scripts = () => {
-        return src('./src/js/main.js')
-            .pipe(sourcemaps.init())
-            .pipe(concat('main.min.js'))
-            .pipe(sourcemaps.write('.'))
-            .pipe(dest('./dist/js'))
-            .pipe(browserSync.stream());
-    },
+const server = () => browserSync.init({
+    server: {
+        baseDir: 'dist'
+    }
+})
 
-//переносим зображення в dist
-    images = () => {
-        return src('./src/img/**.*')
-            .pipe(dest('./dist/img'))
-    },
 
-// робота із шрифтами
-    fonts = () => {
-        return src('./src/fonts/**.ttf')
-            .pipe(ttf2woff2())
-            .pipe(dest('./dist/fonts/'))
-    },
+const watcher = () => {
+    watch('./src/**/*.html', html)
+    watch(['./src/**/*.css', './src/**/*.scss', './src/**/*.sass'], styles)
+    watch('./src/js/**/**.js', scripts)
+    watch('./src/img/**.*', images)
+    watch('.src/fonts/**.ttf', fonts)
+};
 
-//перезапис папки dist при перезавантаженні
-    clear = () => del('./dist'),
-
-//live сервер
-    server = () => browserSync.init({server: {baseDir: 'dist'}}),
-
-//спостереження за змінами у файлах
-    watcher = () => {
-        watch('./src/**/*.html', html)
-        watch(['./src/**/*.css', './src/**/*.scss', './src/**/*.sass'], styles)
-        watch('./src/js/**/**.js', scripts)
-        watch('./src/img/**.*', images)
-        watch('.src/fonts/**.ttf', fonts)
-    };
-
-//таски
 exports.html = html;
 exports.watch = watch;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
 
-//live сбірка при змінах | черга запуску тасків
+
 exports.default = series(
     clear,
     html,
@@ -104,3 +103,34 @@ exports.default = series(
     images,
     parallel(watcher, server)
 );
+
+
+//BUILD TASKS--------------------------
+const clearBuild = () => del('./build')
+
+    const htmlBuild = () => {
+        return src('./dist/*.html')
+            .pipe(dest('./build'));
+    }
+
+const stylesBuild = () => {
+    return src(
+            './dist/css/main.css')
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(dest('./build/css'))
+}
+
+const fontsBuild = () => {
+    return src('./dist/fonts/**.woff2')
+        .pipe(ttf2woff2())
+        .pipe(dest('./build/fonts/'))
+}
+
+exports.build = series(
+    clearBuild,
+    htmlBuild,
+    stylesBuild,
+    fontsBuild
+)
